fd9f6b254801f4be3ac03bef8456d38ba9949152
David Moure <moure96@gmail.com>
2025-12-10 11:35:06 +0100

    Ejercicio acabado

--- Entorno-Cliente/Tema3/T3Ejemplos/T3Ex15 copy/chat-cliente.js
+++ Entorno-Cliente/Tema3/T3Ejemplos/T3Ex15 copy/chat-cliente.js
@@ -1,62 +1,103 @@
 
-document.addEventListener('DOMContentLoaded',()=>{
-    mostrarMensaje('Intentando conectarse al servidor localhost:8082')
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarMensajeInformacion('Intentando conectarse al servidor localhost:8082')
+    conectar()
 })
 // Créase unha nova conexión ao servidor a través dun WebSocket
-const socketCliente = new WebSocket("ws://localhost:8082");
+let socketCliente
 // Esta liña execútase cada vez que se produce o evento de conexión a un servidor.
-socketCliente.addEventListener("open", () => {
-console.log("Conectado ao servidor.");
-// Enviamos unha mensaxe ao servidor.
+function conectar() {
+    socketCliente = new WebSocket("ws://localhost:8082");
+    socketCliente.addEventListener("open", () => {
+        console.log("Conectado ao servidor.");
+        mostrarMensajeInformacion("Conectado con el servidor")
+        btnEnviar.removeAttribute('disabled')
+        // Enviamos unha mensaxe ao servidor.
 
-estado.classList.toggle('desconectado')
-estado.classList.toggle('conectado')
-estado.value='conectado'
-socketCliente.send("Ola, Servidor!");
-});
-// Nesta función recibimos mensaxes do servidor.
-socketCliente.addEventListener("message", (mensaxe) => {
-    mostrarMensaje(mensaxe.data)
-console.log("O servidor enviou a seguinte mensaxe: "+mensaxe.data);
-});
+        estado.classList.toggle('desconectado')
+        estado.classList.toggle('conectado')
+        estado.innerHTML = 'CONECTADO'
+        socketCliente.send("Ola, Servidor!");
+    });
+    // Nesta función recibimos mensaxes do servidor.
+    socketCliente.addEventListener("message", (mensaxe) => {
+        mostrarMensajeServidor("O servidor enviou a seguinte mensaxe: <br>" + mensaxe.data)
+        console.log("O servidor enviou a seguinte mensaxe: <br>" + mensaxe.data);
+    });
 
-socketCliente.onclose=(evento)=>{
-    if (evento.wasClean){
-        alert(`[Close] conexión cerrada limpiamente, codigo=${evento.code} motivo ${evento.reason}` )
-    }else{
-        alert(`[Close] La conexión cesó inesperadamente`)
+    socketCliente.onclose = (evento) => {
+        if (evento.wasClean) {
+            alert(`[Close] conexión cerrada limpiamente, codigo=${evento.code} motivo ${evento.reason}`)
+            mostrarMensajeInformacion(`[Close] conexión cerrada limpiamente, codigo=${evento.code} motivo ${evento.reason}`)
+        } else {
+            alert(`[Close] La conexión cesó inesperadamente`)
 
+        }
     }
-}
-//Cuando no esta disponible
-socketCliente.onerror= ()=>{
-console.log("El servidor no esta disponible")
-};
-
-const estado=document.getElementById('estado')
-const mensajeInput=document.getElementById('input-mensaxe')
-const log=document.getElementById('log-mensaxes')
-const btnEnviar=document.getElementById('btn-enviar')
-const btnConexion=document.getElementById('btn-conexion')
-
-
-
-function enviarMensaje(){
-let mensaje=mensajeInput.value.trim()
-socketCliente.send(mensaje);
-
+    //Cuando no esta disponible
+    socketCliente.onerror = () => {
+        console.log("El servidor no esta disponible")
+        mostrarMensajeInformacion("El servidor no esta disponible")
+    };
 }
 
-function xestionarConexion(){
+const estado = document.getElementById('estado')
+const mensajeInput = document.getElementById('input-mensaxe')
+const log = document.getElementById('log-mensaxes')
+const btnEnviar = document.getElementById('btn-enviar')
+const btnConexion = document.getElementById('btn-conexion')
+
+
+
+function enviarMensaxe() {
+    let mensaje = mensajeInput.value.trim()
+    socketCliente.send(mensaje);
+    mostrarMensajeCliente('El cliente ha dicho:<br> ' +  mensaje)
 
 }
-function mostrarMensaje(text){
-    let div=document.createElement('div')
-    div.innerHTML=text
-    let br=document.createElement('br')
+
+function xestionarConexion() {
+    if (socketCliente.readyState === WebSocket.OPEN) {
+
+        socketCliente.close()
+        btnConexion.innerHTML = 'Abrir Conexión'
+    }
+    else {
+        btnConexion.innerHTML = 'Pechar Conexión'
+        conectar()
+    }
+    btnEnviar.toggleAttribute('disabled')
+
+}
+
+
+function mostrarMensajeServidor(text){
+ let div = document.createElement('div')
+    div.innerHTML = text
+    div.classList.add("servidor")
+    let br = document.createElement('br')
     log.appendChild(div)
     log.appendChild(br)
-
 }
+function mostrarMensajeCliente(text){
+ let div = document.createElement('div')
+    div.innerHTML = text
+    div.classList.add("cliente")
+    let br = document.createElement('br')
+    log.appendChild(div)
+    log.appendChild(br)
+}
+function mostrarMensajeInformacion(text){
+ let div = document.createElement('div')
+ div.classList.add("informacion")
+    div.innerHTML = text
+    let br = document.createElement('br')
+    log.appendChild(div)
+    log.appendChild(br)
+}
+
+
+
+
 
 
