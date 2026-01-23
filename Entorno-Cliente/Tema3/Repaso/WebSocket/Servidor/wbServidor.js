f66c7aaf775d7554a616bd235f5622be89e7ff2a
David Moure <moure96@gmail.com>
2026-01-23 15:18:28 +0100

    Ejercicio Subasta

---
+++ Entorno-Cliente/Tema3/Repaso/WebSocket/Servidor/wbServidor.js
@@ -0,0 +1,132 @@
+let usuarios = []
+let idIntervalo
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8082 });
+class Usuario {
+    constructor(id, nombre, puja = 0) {
+        this._id = id
+        this._nombre = nombre
+        this._puja = puja
+    }
+    static crearUsuario(usuario) {
+        return new Usuario(usuario._id, usuario._nombre, usuario._puja)
+    }
+    comprobarPuja(puja) {
+
+        return (this._puja < puja) ? true : false
+    }
+    subirPuja(puja) {
+
+
+        if (this.comprobarPuja(puja)) {
+            this._puja = puja
+            console.log("Puja actualizada a:", puja)
+        } else {
+            console.log("Puja no actualizada")
+        }
+
+    }
+}
+
+
+class Objeto {
+    constructor(nombre) {
+        this._nombre = nombre
+        this._pujaActual = 0
+        this._usuario = {}
+        this._cuentaAtras = 60
+        this._terminada = false
+
+    }
+    set usuarioGanadorSetter(usuario) {
+        console.log(usuario)
+        this._usuario = usuario
+    }
+    set cuentaAtrasSetter(tiempo) {
+        this._cuentaAtras = tiempo
+        if (this._cuentaAtras < 0) {
+            this._terminada = true
+        }
+    }
+    get cuentaAtrasGetter() {
+        return this._cuentaAtras
+    }
+    set pujaActualSetter(puja) {
+        this._pujaActual = puja
+    }
+    incrementarTiempoIntervalo() {
+        this._cuentaAtras += 15
+    }
+}
+
+
+function iniciarIntervalo() {
+
+    if (idIntervalo) {
+        clearInterval(idIntervalo)
+    }
+    idIntervalo = setInterval(() => {
+
+        // console.log(jarron)
+        enviarAtodos(JSON.stringify(jarron))
+        jarron.cuentaAtrasSetter = jarron.cuentaAtrasGetter -= 1
+        if (jarron.cuentaAtrasGetter <= 0) {
+            clearInterval(idIntervalo)
+        }
+    }, 1000)
+}
+function enviarAtodos(mensaje) {
+
+    wss.clients.forEach(client => {
+        if (client.readyState == WebSocket.OPEN) {
+            client.send(mensaje)
+        }
+    })
+}
+
+const jarron = new Objeto("Jarron Medieval")
+
+
+
+console.log("Servidor iniciado")
+
+wss.on("connection", ws => {
+    console.log("Se conecto un cliente!")
+    iniciarIntervalo()
+
+
+    ws.on("message", mensaje => {
+        try {
+            let data = mensaje.toString().trim()
+            let dataParsed = JSON.parse(data)
+            console.log("Data recibida", dataParsed)
+            if (usuarios.some((usuarioArr) => usuarioArr._id == dataParsed._id)) {
+                console.log("Entro en la validacion de la puja")
+
+                const usuarioPuja = usuarios.find((usuarioArr) => usuarioArr._id == dataParsed._id)
+                usuarioPuja.subirPuja(Number.parseFloat(dataParsed._puja))
+                jarron.usuarioGanadorSetter = Usuario.crearUsuario(usuarioPuja)
+                jarron.pujaActualSetter = usuarioPuja._puja
+                if (jarron.cuentaAtrasGetter <= 20) {
+
+                    jarron.incrementarTiempoIntervalo()
+                }
+
+            } else {
+
+                let usuario = Usuario.crearUsuario(dataParsed)
+                console.log("Usuario Creado:", usuario)
+                usuarios.push(usuario)
+            }
+            console.log(usuarios)
+            // ws.send("Hola cliente")
+        } catch (error) {
+            console.log("Mensaje recibido")
+
+        }
+    });
+    ws.on("close", () =>
+        console.log("El cliente se desconectó."))
+})
+
---
+++ Entorno-Cliente/Tema3/Repaso/WebSocket/script.js
@@ -0,0 +1,132 @@
+const ws = new WebSocket("ws://localhost:8082")
+ws.addEventListener("open", () => {
+    console.log("Conectado al servidor!")
+
+    ws.send("Hola, Servidor!!")
+});
+
+ws.addEventListener("message", ({ data }) => {
+    try {
+        dataParsed = JSON.parse(data)
+        renderizarData(dataParsed)
+    } catch (error) {
+        console.log("El servidor envio: ", data)
+    }
+})
+
+
+const objeto = document.getElementById("objeto")
+const contador = document.getElementById("contador")
+const pujaMarcador = document.getElementById("puja")
+const divNombre = document.getElementById("divNombre")
+const inputNombre = document.getElementById("ipt-nombre")
+const inputPuja = document.getElementById("ipt-puja")
+const divMostrador=document.querySelector(".container-mostrador")
+
+let usuario
+class Usuario {
+    constructor(id, nombre, puja = 0) {
+        this._id = id
+        this._nombre = nombre
+        this._puja = puja
+    }
+    comprobarPuja(puja) {
+
+        return (this._puja < puja) ? true : false
+    }
+    subirPuja(puja) {
+
+        if (this.comprobarPuja(puja)) {
+            this._puja = puja
+            console.log("Puja actualizada a:", puja)
+            enviarMensaje(JSON.stringify(usuario))
+        } else {
+
+            console.log("Puja no actualizada")
+        }
+
+    }
+}
+function enviarMensaje(mensaje) {
+    console.log("METODO: Mensaje enviado al servidor:", mensaje)
+    ws.send(mensaje)
+}
+
+
+
+
+
+
+
+function renderizarData(dataParsed) {
+    if (dataParsed._terminada == true) {
+        console.log("Subasta terminada")
+    }
+    objeto.innerHTML =/*html*/`
+        Subasta de: <strong> ${dataParsed._nombre}</strong>
+    `
+    let tiempoRestante = dataParsed._cuentaAtras
+    contador.innerHTML = `${dataParsed._cuentaAtras} s`
+    contador.classList.add("cuenta-atras")
+
+    if (tiempoRestante <= 20) {
+        contador.classList.add("parpadear")
+    } else {
+        contador.classList.remove("parpadear")
+
+    }
+    
+    if(dataParsed._terminada==true){
+        pujaMarcador.innerHTML = `${dataParsed._usuario._nombre} <br>${dataParsed._pujaActual} €`
+
+        divMostrador.innerHTML=/*html*/
+        `<h2>La subasta ha terminado </h2>
+        <br>
+        <p> El ganador del ${dataParsed._nombre} ha sido: ${dataParsed._usuario._nombre} por un valor de <strong>${dataParsed._pujaActual}</strong> euros</p>
+        `
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+inputNombre.addEventListener("keypress", (ev) => {
+    if (ev.key == "Enter") {
+        console.log("Tu nombre es:", inputNombre.value)
+        divNombre.classList.add("oculto")
+        let id = new Date().valueOf()
+        usuario = new Usuario(id, inputNombre.value)
+        console.log("Usuario creado: ", usuario)
+        enviarMensaje(JSON.stringify(usuario))
+
+    }
+})
+inputPuja.addEventListener("keypress", (ev) => {
+    if (ev.key == "Enter") {
+        if (usuario instanceof Usuario) {
+            console.log("Tu puja es:", inputPuja.value)
+            usuario.subirPuja(+inputPuja.value)
+
+        } else {
+            contador.innerHTML = "Necesitas tener un usuario para pujar!!!"
+        }
+
+    }
+})
+
+
+
+
